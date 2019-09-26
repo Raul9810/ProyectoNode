c707cdc2f5f88e42ef28133681461284d76cefef
Raul <r_mp22@hotmail.com>
2019-09-26 17:07:17 -0500

    JSON

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 var app = express();
+var arreglo = [], a1 = [], a2 = [];
+var otro = {}
 
 app.get("/", (req,res)=>{
-    console.log(req);
-    res.send("Que onda Fertss");
+    res.send();
+})
+
+app.get("/:nombre/:edad", (req,res)=>{
+    var nombre = req.params.nombre
+    var edad = req.params.edad
+    if(req.url != '/favicon.ico'){
+        otro[nombre] = edad
+        res.send();
+        console.log(otro);
+    }
+})  
+
+app.get("/:id", (req,res)=>{
+    if(req.url != '/favicon.ico'){
+        arreglo.push(req.params.id)
+        res.send();
+        console.log(arreglo);
+    }
 })
 
 app.listen(3000,() => {
